27f8567647cb2b31fdfb14d0eb955eb0e3cf270f
nithya.shenbagam05@gmail.com <nithya.shenbagam05@gmail.com>
2022-02-11 02:54:14 +0000

    yes

---
+++ models/DbtPractise/TimeIncr.sql
@@ -0,0 +1,16 @@
+{{config(materialized='incremental', unique_key = 't_time')}}
+
+
+select *,
+to_time(concat(T_HOUR::varchar, ':', T_MINUTE, ':', T_SECOND)) as time_concat
+from
+SNOWFLAKE_SAMPLE_DATA.TPCDS_SF10TCL.TIME_DIM td
+where time_concat <= current_time
+and td.T_AM_PM = {{ var('AMPMvariable')}}
+
+{%if is_incremental() %}
+
+and time_concat > (select max(time_concat) from {{ this }})
+
+{%endif%}
+
